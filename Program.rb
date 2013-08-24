5c1dd25a097fd262aeac5eec8ddfe3f1c6cb43c5
Jhoan <jhoan-mac@hotmail.co.uk>
2013-08-23 22:36:19 -0500

    Finished testing for function explode

--- Program.rb
+++ Program.rb
@@ -1,12 +1,18 @@
 #Class: Program
 class Program
 	#We'll store the ocurrences in hashes: Name=>Count
-	@headers = Hash.new()
+	@code = Array.new()
+	@headers = Hash.new() 
 	@defines = Hash.new()
-	@functions = Hash.new()
+	@functions = Hash.new() 
 	@vars = Hash.new() #Main variables only
-	def initialize(code)
-		raise ArgumentError, "Expected Array but got #{code.class.name}" unless code.kind_of? Array
+	def initialize(new_code)
+		raise ArgumentError, "Expected Array but got #{new_code.class.name}" unless new_code.kind_of? Array
+	end
+	def explode! #Converts the array containing the code into an array of arrays where every inner array is a function
+		
+	end
+	def get_vars(function) #Get the variable declarations of a function
 	end
 
 end
--- test.rb
+++ test.rb
@@ -1,4 +1,46 @@
 #This file is for testing purposes only
-2.times do |i|
-	puts i
+code = Array.new(["#include<stdio.h>",
+				"float foo(int,char)",
+				"int main(){",
+					"int x=0","float y=3",
+					"for(){",
+						"code",
+						"do{",
+							"more code",
+						"}while",
+						"yet more code",
+					"}",
+							
+				"return 0",
+				"}",
+				"float foo(int n,char d){",
+					"code",
+				"}"])
+code.each { |line| puts line }
+puts "***************"
+brace_count = 0
+output = Array.new()
+output.push(Array.new)
+i = 0
+code.each do |current_line| 
+	#puts current_line
+	if current_line.include? "{" 
+		brace_count+=1
+		if brace_count == 1
+			output.push(Array.new)
+			i+=1
+		end
+	end	
+	
+	if current_line.include? "}"
+		brace_count -= 1
+	end
+	
+	output[i].push(current_line)
+end
+
+output.each do |arr|  
+	puts "*****"
+	arr.each { |line| puts line }
+	puts "*****"
 end
